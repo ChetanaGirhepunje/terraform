d795eae52bad53e7750a4d651194825e07d8de06
ChetanaGirhepunje <144361398+ChetanaGirhepunje@users.noreply.github.com>
2025-12-23 12:08:13 +0530

    vpc.tf file added

---
+++ vpc/vpc.tf
@@ -0,0 +1,8 @@
+module "vpc" {
+  source = "./modules"
+  region = "ap-south-1"
+  project                = "cdecb45"
+  vpc_cidr_block         = "10.0.0.0/16"
+  vpc_cidr_pub_subnet    = "10.0.1.0/24"
+  vpc_cidr_pri_subnet    = "10.0.2.0/24"
+}
