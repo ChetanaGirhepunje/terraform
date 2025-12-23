e683320c0e5491269fcaadc8974430cf886a74c3
ChetanaGirhepunje <144361398+ChetanaGirhepunje@users.noreply.github.com>
2025-12-23 23:27:25 +0530

    rds.tf file added

---
+++ rds/rds.tf
@@ -0,0 +1,16 @@
+module "db" {
+   source = "./modules"
+   region = "ap-south-2"
+   engine = "mariadb"
+   engine_version = "11.4.8"
+   instance_class = "db.t3.micro"
+   username = "chetana"
+   password = "chetana@123"
+   db_name = "cg_db"
+   name = "cg_sg"
+   subnet_ids = [
+    "subnet-00e6319a0a88945ee",
+    "subnet-03fe18304f51a1530"
+   ]
+   vpc_security_group_ids = ["sg-09040ae4cd8504987"]
+} 
